e48c3beb186dbba83dbd40f35a78215e2b1dfd31
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2018-01-18 11:47:28 +0100

    Overlap sidebar when using phone

--- docs/static/content.js
+++ docs/static/content.js
@@ -578,15 +578,15 @@ function ctor_structure()
   self.dataPath = scriptDir + '/source/data_translate.js';
   self.metaViewport = '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">';
   self.template = '<div id="body">' +
-  '<div id="head"><div class="h-tabs"><ul><li data-translate data-content="Content"></li><li data-translate data-content="Index"></li><li data-translate data-content="Search"></li></ul></div><div class="dragbar"></div><div class="h-tools"><div class="main"><ul><li class="sidebar" title="Hide/Show sidebar" data-translate>&#926;</li></ul></div><div class="online"><ul><li class="home" title="Home page" data-translate><a href="' + location.protocol + '//' + location.host + '">&#916;</a></li></ul><ul><li class="language" title="Change language" data-translate data-content="en"></li></ul><ul class="languages"><li class="arrow">&#9658;</li><li><a title="English" data-content="en"></a></li><li><a title="Deutsch (German)" data-content="de"></a></li><li><a title="&#x4E2D;&#x6587; (Chinese)" data-content="zh"></a></li></ul><ul><li class="version" title="Change AHK version" data-translate data-content="v1"></li></ul><ul class="versions"><li class="arrow">&#9658;</li><li><a title="AHK v1.1" data-content="v1"></a></li><li><a title="AHK v2.0" data-content="v2"></a></li></ul><ul><li class="edit" title="Edit page on GitHub" data-translate><a data-content="E"></a></li></ul></div><div class="chm"><ul><li class="back" title="Go back" data-translate>&#9668;</li><li class="forward" title="Go forward" data-translate>&#9658;</li><li class="zoom" title="Change font size" data-translate data-content="Z"></li><li class="print" title="Print current document" data-translate data-content="P"></li></ul></div></div></div>' +
+  '<div id="head"><div class="h-tabs"><ul><li data-translate data-content="Content"></li><li data-translate data-content="Index"></li><li data-translate data-content="Search"></li></ul></div><div class="h-tools"><div class="main"><ul><li class="sidebar" title="Hide/Show sidebar" data-translate>&#926;</li></ul></div><div class="online"><ul><li class="home" title="Home page" data-translate><a href="' + location.protocol + '//' + location.host + '">&#916;</a></li></ul><ul><li class="language" title="Change language" data-translate data-content="en"></li></ul><ul class="languages"><li class="arrow">&#9658;</li><li><a title="English" data-content="en"></a></li><li><a title="Deutsch (German)" data-content="de"></a></li><li><a title="&#x4E2D;&#x6587; (Chinese)" data-content="zh"></a></li></ul><ul><li class="version" title="Change AHK version" data-translate data-content="v1"></li></ul><ul class="versions"><li class="arrow">&#9658;</li><li><a title="AHK v1.1" data-content="v1"></a></li><li><a title="AHK v2.0" data-content="v2"></a></li></ul><ul><li class="edit" title="Edit page on GitHub" data-translate><a data-content="E"></a></li></ul></div><div class="chm"><ul><li class="back" title="Go back" data-translate>&#9668;</li><li class="forward" title="Go forward" data-translate>&#9658;</li><li class="zoom" title="Change font size" data-translate data-content="Z"></li><li class="print" title="Print current document" data-translate data-content="P"></li></ul></div></div></div>' +
   '<div id="main"><div id="left"><div class="toc"></div><div class="index"><div class="label" data-translate data-content="Type in the keyword to find:"></div><div class="input"><input type="text" /></div><div class="list"></div></div><div class="search"><div class="label" data-translate data-content="Type in the word(s) to search for:"></div><div class="input"><input type="text" /></div><div class="list"></div></div></div><div class="dragbar"></div><div id="right" tabIndex="-1"><div class="area">';
   self.template = isIE || isEdge ? self.template.replace(/ data-content="(.*?)">/g, '>$1') : self.template;
   self.build = function() { document.write(self.template); }; // Write HTML before DOM is loaded to prevent flickering.
   self.modify = function() { // Modify elements added via build.
 
-    // --- Hide sidebar if using a mobile browser ---
+    // --- If phone, hide and overlap sidebar ---
 
-    if (isPhone) { self.displaySidebar(false); }
+    if (isPhone) { self.displaySidebar(false); $('#left').addClass('phone') }
 
     // --- Add events ---
 
@@ -853,6 +853,7 @@ function ctor_structure()
       {
         cache.sidebarWidth = e.pageX+2;
         $('#left').add('#head div.h-tabs').width(cache.sidebarWidth);
+        $('div.dragbar').css('left', cache.sidebarWidth);
         $('#frame').fadeIn();
         $('div.ghostbar').remove();
         $(document).unbind('mousemove');
@@ -867,11 +868,11 @@ function ctor_structure()
     var $leftArea = $('#left');
     var $dragbar = $('.dragbar');
     var hide = {width: 0, visibility: "hidden"};
-    var show = {width: cache.sidebarWidth+"px", visibility: "visible"};
+    var show = {width: cache.sidebarWidth, visibility: "visible"};
     if (display) {
       $headTabs.css(show);
       $leftArea.css(show);
-      $dragbar.show();
+      $dragbar.show().css('left', cache.sidebarWidth);
       $('input', $leftArea).focus();
     }
     else {
@@ -998,7 +999,7 @@ function ctor_structure()
       postMessageToFrame('updateCache', [{LastUsedSource: cache.LastUsedSource, search: {input: cache.search.input}}]);
       postMessageToFrame('changeURL', [url]);
       if (isPhone)
-        setTimeout(function() { self.displaySidebar(false); postMessageToFrame('changeURL', [url]); }, 200);
+        setTimeout(function() { self.displaySidebar(false); }, 200);
     }
     else {
       if (isPhone) {
