a3dc3ead5630613ab2d2be2d3b8ce4bcfd3e3614
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2018-05-23 12:42:19 +0200

    Update frame cache when setting a cache prop

--- docs/static/content.js
+++ docs/static/content.js
@@ -30,9 +30,16 @@ var cache = {
   displaySidebar: user.displaySidebar,
   sidebarWidth: '18em',
   RightIsFocused: true,
-  toc: {clickItem: 0, scrollPos: 0},
-  index: {input: "", clickItem: 0, scrollPos: 0},
-  search: {data: {}, highlightWords: false, input: "", clickItem: 0, scrollPos: 0},
+  toc_clickItem: 0,
+  toc_scrollPos: 0,
+  index_input: "",
+  index_clickItem: 0,
+  index_scrollPos: 0,
+  search_data: {},
+  search_highlightWords: false,
+  search_input: "",
+  search_clickItem: 0,
+  search_scrollPos: 0,
   load: function() {
     try {
       var data = JSON.parse(window.name);
@@ -46,6 +53,13 @@ var cache = {
   },
   save: function() {
     window.name = JSON.stringify(this);
+  },
+  set: function(prop, value) {
+    this[prop] = value;
+    try {
+      postMessageToFrame('updateCache', [prop, value])
+    } catch(e) {}
+    return value;
   }
 };
 
@@ -96,10 +110,10 @@ var isPhone = (document.documentElement.clientWidth <= 600);
       $('head').append('<style>body {font-size:' + cache.fontSize + 'em}</style>');
       normalizeParentURL = function() {
         postMessageToParent('normalizeURL', [$.extend({}, window.location), document.title, history.state]);
-        if (cache.toc.clickItem)
+        if (cache.toc_clickItem)
           if (supportsHistory)
-            history.replaceState({toc_clickItem: cache.toc.clickItem}, null, null);
-        cache.toc.clickItem = 0;
+            history.replaceState({toc_clickItem: cache.toc_clickItem}, null, null);
+        cache.set('toc_clickItem', 0);
       }
       normalizeParentURL(); $(window).on('hashchange', normalizeParentURL);
       structure.addShortcuts();
@@ -109,7 +123,7 @@ var isPhone = (document.documentElement.clientWidth <= 600);
         $('html').attr({ id: 'right'});
         if (!cache.translate)
           loadScript(structure.dataPath, function() {
-            cache.translate = translateData;
+            cache.set('translate', translateData);
             addFeatures();
           });
         else
@@ -119,7 +133,10 @@ var isPhone = (document.documentElement.clientWidth <= 600);
         var data = JSON.parse(event.originalEvent.data);
         switch(data[0]) {
           case 'updateCache':
-          $.extend(true, cache, data[1]);
+          if(typeof data[1] === 'object')
+            $.extend(cache, data[1]);
+          else
+            cache[data[1]] = data[2];
           break;
 
           case 'highlightWords':
@@ -216,7 +233,7 @@ var isPhone = (document.documentElement.clientWidth <= 600);
   // Get the data if needed and modify the site:
   if (!cache.translate)
     loadScript(structure.dataPath, function() {
-      cache.translate = translateData;
+      cache.set('translate', translateData);
       structure.modify();
       if (!isFrameCapable)
         $(document).ready(addFeatures);
@@ -226,25 +243,25 @@ var isPhone = (document.documentElement.clientWidth <= 600);
     if (!isFrameCapable)
       $(document).ready(addFeatures);
   }
-  if (!cache.toc.data)
+  if (!cache.toc_data)
     loadScript(toc.dataPath, function() {
-      cache.toc.data = tocData;
+      cache.set('toc_data', tocData);
       toc.modify();
     });
   else
     toc.modify();
-  if (!cache.index.data)
+  if (!cache.index_data)
     loadScript(index.dataPath, function() {
-      cache.index.data = indexData;
+      cache.set('index_data', indexData);
       index.modify();
     });
   else
     index.modify();
-  if (!cache.search.index || !cache.search.files || !cache.search.titles)
+  if (!cache.search_index || !cache.search_files || !cache.search_titles)
     loadScript(search.dataPath, function() {
-      cache.search.index = SearchIndex;
-      cache.search.files = SearchFiles;
-      cache.search.titles = SearchTitles;
+      cache.set('search_index', SearchIndex);
+      cache.set('search_files', SearchFiles);
+      cache.set('search_titles', SearchTitles);
       search.modify();
     });
   else
@@ -280,7 +297,7 @@ function ctor_toc()
   };
   // --- Modify the elements of the TOC tab ---
   self.modify = function() {
-    $toc = $('#left div.toc').html(self.create(cache.toc.data));
+    $toc = $('#left div.toc').html(self.create(cache.toc_data));
     $tocList = $('li > span', $toc);
     // --- Fold items with subitems ---
 
@@ -291,8 +308,8 @@ function ctor_toc()
     // Select the item on click:
     $tocList.on("click", function() {
       $this = $(this);
-      cache.toc.clickItem = $tocList.index(this);
-      cache.toc.scrollPos = $toc.scrollTop();
+      cache.set('toc_clickItem', $tocList.index(this));
+      cache.set('toc_scrollPos', $toc.scrollTop());
       // Fold/unfold item with subitems:
       if ($this.parent().has("ul").length) {
         $this.siblings("ul").slideToggle(100);
@@ -335,7 +352,7 @@ function ctor_toc()
   };
   self.preSelect = function($toc, url, relPath) { // Apply stored settings.
     var tocList = $('li > span', $toc);
-    var clicked = tocList.eq(cache.toc.clickItem);
+    var clicked = tocList.eq(cache.toc_clickItem);
     var relPathNoHash = relPath.replace(url.hash,'');
     var found = null;
     var foundList = [];
@@ -360,14 +377,14 @@ function ctor_toc()
     if (clicked.is(found))
       el = clicked;
     else
-      cache.toc.scrollPos = ""; // Force calculated scrolling.
+      cache.set('toc_scrollPos', ""); // Force calculated scrolling.
     // If items are found:
     if (el) {
       // Highlight items and parents:
       self.deselect($toc); el.trigger('select');
       // Scroll to the last match:
-      if (cache.toc.scrollPos != "" || cache.toc.scrollPos != 0)
-        $toc.scrollTop(cache.toc.scrollPos);
+      if (cache.toc_scrollPos != "" || cache.toc_scrollPos != 0)
+        $toc.scrollTop(cache.toc_scrollPos);
       if (!isScrolledIntoView(el, $toc)) {
         el[el.length-1].scrollIntoView(false);
         $toc.scrollTop($toc.scrollTop()+100);
@@ -399,14 +416,14 @@ function ctor_index()
   self.modify = function() { // Modify the elements of the index tab.
     var $index = $('#left div.index');
     var $indexInput = $('.input input', $index);
-    var $indexList = $('div.list', $index).html(self.create(cache.index.data));
+    var $indexList = $('div.list', $index).html(self.create(cache.index_data));
 
     // --- Hook up events ---
 
     // Select closest index entry and show color indicator on input:
     $indexInput.on('keyup', function() { // keyup instead of input due IE8.
       var $this = $(this);
-      var input = cache.index.input = $this.val().toLowerCase();
+      var input = cache.set('index_input', $this.val().toLowerCase());
       // if no input, remove color indicator and return:
       if (!input) {
         $this.removeAttr('class');
@@ -445,9 +462,9 @@ function ctor_index()
     return match;
   };
   self.preSelect = function($indexList, $indexInput) { // Apply stored settings.
-    var clicked = $indexList.children().eq(cache.index.clickItem);
-    $indexInput.val(cache.index.input);
-    $indexList.scrollTop(cache.index.scrollPos);
+    var clicked = $indexList.children().eq(cache.index_clickItem);
+    $indexInput.val(cache.index_input);
+    $indexList.scrollTop(cache.index_scrollPos);
     clicked.click();
   };
 }
@@ -469,7 +486,7 @@ function ctor_search()
     // Refresh the search list and show color indicator on input:
     $searchInput.on('keyup', function() { // keyup instead of input due IE8.
       var $this = $(this);
-      var input = cache.search.input = $this.val();
+      var input = cache.set('search_input', $this.val());
       // if no input, empty the search list, remove color indicator and return:
       if (!input) {
         $searchList.empty();
@@ -477,13 +494,13 @@ function ctor_search()
         return;
       }
       // Otherwise fill the search list:
-      cache.search.data = self.create(input);
-      $searchList.html(cache.search.data);
+      cache.set('search_data', self.create(input));
+      $searchList.html(cache.search_data);
       // Select the first item and add color indicator:
       var searchListChildren = $searchList.children();
       if (searchListChildren.length) {
         searchListChildren.first().click();
-        cache.search.clickItem = 0;
+        cache.set('search_clickItem', 0);
         $this.attr('class', 'match'); // 'items found'
       }
       else
@@ -493,11 +510,11 @@ function ctor_search()
     setTimeout( function() { self.preSelect($searchList, $searchInput, $searchCheckBox); }, 0);
   };
   self.preSelect = function($searchList, $searchInput, $searchCheckBox) { // Apply stored settings.
-    $searchInput.val(cache.search.input);
-    $searchList.html(cache.search.data);
-    $searchList.scrollTop(cache.search.scrollPos);
-    $searchList.children().eq(cache.search.clickItem).click();
-    $searchCheckBox.prop('checked', cache.search.highlightWords);
+    $searchInput.val(cache.search_input);
+    $searchList.html(cache.search_data);
+    $searchList.scrollTop(cache.search_scrollPos);
+    $searchList.children().eq(cache.search_clickItem).click();
+    $searchCheckBox.prop('checked', cache.search_highlightWords);
   };
   self.convertToArray = function(SearchText) { // Convert text to array.
     // Normalize whitespace:
@@ -557,7 +574,7 @@ function ctor_search()
         if (!file_has_all_words(w[i], qry, 1))
           continue // Skip files which don't have all the words.
         c++
-        var f = cache.search.files[w[i]]
+        var f = cache.search_files[w[i]]
         // data.files excludes '.htm' to save space, so add it back:
         if (f.indexOf('#') != -1)
           f = f.replace('#', '.htm#')
@@ -565,7 +582,7 @@ function ctor_search()
           f = f + '.htm'
         // var ret_i = { u: location.href.replace(/\/docs\/.*/, "/docs/" + f),
         var ret_i = { u: f,
-              t: (cache.search.titles[w[i]] || f) }
+              t: (cache.search_titles[w[i]] || f) }
         ret.push(ret_i)
       }
       return ret
@@ -681,10 +698,10 @@ function ctor_search()
     function index_whole(word) {
       // Return a word from the index, decoding the list of files
       // if it hasn't been decoded already:
-      var files = cache.search.index[word]
+      var files = cache.search_index[word]
       if (typeof(files) == "string") {
         files = decode_numbers(files)
-        cache.search.index[word] = files
+        cache.search_index[word] = files
       }
       return files
     }
@@ -700,7 +717,7 @@ function ctor_search()
       // and cache the result.
       files = []
       var files_low = []
-      for (iw in cache.search.index) {
+      for (iw in cache.search_index) {
         var p = iw.indexOf(word)
         if (p != -1)
           files.push.apply(p == 0 ? files : files_low, index_whole(iw))
@@ -843,11 +860,10 @@ function ctor_structure()
     $('li.forward', $chm).on('click', function() { history.forward(); });
     // 'Zoom' button:
     $('li.zoom', $chm).on('click', function() {
-      cache.fontSize += 0.2;
+      cache.set('fontSize', cache.fontSize + 0.2);
       if (cache.fontSize > 1.4)
-        cache.fontSize = 0.6;
+        cache.set('fontSize', 0.6);
       $('#frame').contents().find('body').css('font-size', cache.fontSize + 'em');
-      postMessageToFrame('updateCache', [{fontSize: cache.fontSize}]);
     });
     // 'Print' button:
     $('li.print', $chm).on('click', function() { window.parent.document.getElementById('frame').contentWindow.document.execCommand('print', false, null); });
@@ -872,7 +888,7 @@ function ctor_structure()
     ListBox.on('scroll', function() {
       var $this = $(this);
       var $parent = $this.parent();
-      cache[$parent.attr('class')].scrollPos = $this.scrollTop();
+      cache.set($parent.attr('class') + '_scrollPos', $this.scrollTop());
     });
     // Select the item on click and scroll to it:
     ListBox.on('click', '> a', function() {
@@ -901,7 +917,7 @@ function ctor_structure()
         var $grandparent = $parent.parent();
         // Store the item's index relative to its parent:
         var className = $grandparent.attr('class');
-        cache[className].clickItem = $this.index();
+        cache.set(className + '_clickItem', $this.index());
         self.openSite($this.attr('href'));
       }
     }).on('touchmove', function() {
@@ -988,23 +1004,17 @@ function ctor_structure()
     $('#left .checkbox input#highlightWords').on('change', function() {
       if(this.checked)
       {
-        cache.search.highlightWords = true;
+        cache.set('search_highlightWords', true);
         if(isFrameCapable)
-        {
-          postMessageToFrame('updateCache', [{search: {highlightWords: true}}]);
-          postMessageToFrame('highlightWords', [cache.search.input]);
-        }
+          postMessageToFrame('highlightWords', [cache.search_input]);
         else
-          search.highlightWords(cache.search.input);
+          search.highlightWords(cache.search_input);
       }
       else
       {
-        cache.search.highlightWords = false;
+        cache.set('search_highlightWords', false);
         if(isFrameCapable)
-        {
-          postMessageToFrame('updateCache', [{search: {highlightWords: false}}]);
           postMessageToFrame('highlightWords', []);
-        }
         else
           search.highlightWords('');
       }
@@ -1030,7 +1040,7 @@ function ctor_structure()
     $(document).mouseup(function(e) {
       if (dragging) 
       {
-        cache.sidebarWidth = e.pageX+2;
+        cache.set('sidebarWidth', e.pageX+2);
         $('#left').add('#head div.h-tabs').width(cache.sidebarWidth);
         $('div.dragbar').css('left', cache.sidebarWidth);
         $('#frame').fadeIn();
@@ -1042,7 +1052,7 @@ function ctor_structure()
   };
   // Display or hide the sidebar:
   self.displaySidebar = function(display) {
-    cache.displaySidebar = display;
+    cache.set('displaySidebar', display);
     var $headTabs = $('#head div.h-tabs');
     var $leftArea = $('#left');
     var $dragbar = $('.dragbar');
@@ -1063,7 +1073,7 @@ function ctor_structure()
   };
   // Show the specified tab:
   self.showTab = function(pos) {
-    cache.clickTab = pos;
+    cache.set('clickTab', pos);
     var $t = $('#head div.h-tabs li');
     var $s = $('#left > div');
     $t.removeClass('selected')
@@ -1075,7 +1085,7 @@ function ctor_structure()
   // Save cache before leaving site:
   self.saveCacheBeforeLeaving = function() {
     $(window).on('beforeunload', function() {
-      cache.RightIsFocused = $(document.activeElement).closest('#right, #left > div.toc').length;
+      cache.set('RightIsFocused', $(document.activeElement).closest('#right, #left > div.toc').length);
       cache.save();
     });
   }
@@ -1178,14 +1188,13 @@ function ctor_structure()
   // Open new site:
   self.openSite = function(url) {
     if (isFrameCapable) {
-      postMessageToFrame('updateCache', [{clickTab: cache.clickTab, search: {input: cache.search.input}, toc: {clickItem: cache.toc.clickItem}}]);
       document.getElementById('frame').contentWindow.location.href = url;
       if (isPhone)
         setTimeout(function() { self.displaySidebar(false); }, 200);
     }
     else {
       if (isPhone) {
-        cache.displaySidebar = false;
+        cache.set('displaySidebar', false);
         setTimeout(function() {
           self.displaySidebar(false);
           if (location.hash)
@@ -1198,11 +1207,11 @@ function ctor_structure()
 
   self.changeTheme = function() {
     if($('#dark-theme').length) {
-      cache.colorTheme = 0;
+      cache.set('colorTheme', 0);
       $('#dark-theme').remove();
       return;
     }
-    cache.colorTheme = 1;
+    cache.set('colorTheme', 1);
     var style = document.createElement('style');
     style.type = 'text/css';
     style.id = 'dark-theme';
@@ -1402,10 +1411,10 @@ function addFeatures()
 
   // Syntax highlighting:
   if (!isIE8) {
-    if (cache.index.data) {
+    if (cache.index_data) {
         addSyntaxColors(pres);
     } else {
-      loadScript(index.dataPath, function() { cache.index.data = indexData; addSyntaxColors(pres); });
+      loadScript(index.dataPath, function() { cache.set('index_data', indexData); addSyntaxColors(pres); });
     }
   }
   function addSyntaxColors(pres) {
@@ -1421,10 +1430,10 @@ function addFeatures()
         6 - command
     */
     var syntax = [[], [], [], [], [], [], []], dict = {};
-    for(var i = 0; i < cache.index.data.length; i++) {
-      var entry = cache.index.data[i][0];
-      var path = cache.index.data[i][1];
-      var type = cache.index.data[i][2];
+    for(var i = 0; i < cache.index_data.length; i++) {
+      var entry = cache.index_data[i][0];
+      var path = cache.index_data[i][1];
+      var type = cache.index_data[i][2];
       if (typeof type !== 'undefined') {
         if (type == 2 && entry.substr(entry.length - 2) == '()')
           entry = entry.substr(0, entry.length - 2);
@@ -1533,7 +1542,7 @@ function addFeatures()
       if (isLink) {
         var a = document.createElement('a');
         if (isLink == true)
-          a.href = scriptDir + '/../' + cache.index.data[dict[match.toLowerCase()]][1];
+          a.href = scriptDir + '/../' + cache.index_data[dict[match.toLowerCase()]][1];
         else
           a.href = scriptDir + '/../' + isLink;
         a.innerHTML = match;
@@ -1581,8 +1590,8 @@ function addFeatures()
 
   // --- Highlight search words with jQuery Highlight plugin ---
 
-  if (cache.search.highlightWords && cache.clickTab == 2)
-    search.highlightWords(cache.search.input);
+  if (cache.search_highlightWords && cache.clickTab == 2)
+    search.highlightWords(cache.search_input);
 }
 
 // --- Get the working directory of the site ---
