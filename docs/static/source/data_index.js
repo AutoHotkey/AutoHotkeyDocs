9f96610560ae32a69bfb5c8d6598b672e2326e84
Lexikos <unravelin@gmail.com>
2020-06-04 17:44:51 +1000

    Remove ErrorLevel

--- docs/static/source/data_index.js
+++ docs/static/source/data_index.js
@@ -441,7 +441,6 @@ indexData = [
   ["environment variables","Concepts.htm#environment-variables"],
   ["environment variables (change them)","commands/EnvSet.htm"],
   ["EnvSet","commands/EnvSet.htm",2],
-  ["ErrorLevel","misc/ErrorLevel.htm",1],
   ["ErrorStdOut","commands/_ErrorStdOut.htm"],
   ["escape sequence","misc/EscapeChar.htm"],
   ["Escape (Gui event)","objects/GuiOnEvent.htm#Escape"],
