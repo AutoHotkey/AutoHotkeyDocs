678ad8316046566a68f17c9f8630e3f9ad895e75
Lexikos <unravelin@gmail.com>
2021-10-28 18:04:02 +1000

    Fix ClipboardAll

--- docs/static/source/data_index.js
+++ docs/static/source/data_index.js
@@ -297,7 +297,6 @@ indexData = [
   ["Click","commands/Click.htm",2],
   ["Click a mouse button","commands/Click.htm"],
   ["Clipboard","misc/A_Clipboard.htm"],
-  ["ClipboardAll","commands/ClipboardAll.htm",2],
   ["ClipboardAll","commands/ClipboardAll.htm",6],
   ["ClipWait","commands/ClipWait.htm",2],
   ["Clone method (Map)","objects/Map.htm#Clone"],
